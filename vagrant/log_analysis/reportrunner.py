a5f57d39520beaa31e9cae220ac13a4e72267ff0
slilleboe3 <scott.lilleboe@gatech.edu>
2017-08-16 12:11:46 -0700

    query 1 2 done

--- vagrant/log_analysis/reportrunner.py
+++ vagrant/log_analysis/reportrunner.py
@@ -12,9 +12,44 @@ class ReportRunner():
 
     def main(self):
       '''Main method that runs various log analysis reports.'''
-  
-      results = self.run_query('select * from authors')
+
+      query1 = '''
+        select a.title, count(a.title)
+        from articles as a 
+            left join log as l on position(a.slug in l.path) > 0
+        group by a.title
+        order by count(a.title) desc
+      '''      
+
+      results = self.run_query(query1)
       self.pretty_print(results)
+      
+      query2 = '''
+        select au.name, count(au.name)
+        from articles as ar 
+            join authors as au on ar.author = au.id
+            left join log as l on position(ar.slug in l.path) > 0
+        group by au.name
+        order by count(au.name) desc
+      '''      
+
+      results = self.run_query(query2)
+      self.pretty_print(results)
+
+      query3 = '''
+        select au.name, count(au.name)
+        from articles as ar 
+            join authors as au on ar.author = au.id
+            left join log as l on position(ar.slug in l.path) > 0
+        group by au.name
+        order by count(au.name) desc
+      '''      
+
+      results = self.run_query(query3)
+      self.pretty_print(results)
+
+
+
       return 0
 
     def run_query(self, query):
@@ -25,6 +60,7 @@ class ReportRunner():
 
       db = psycopg2.connect(database=self.dbName)
       c = db.cursor()
+      #print(query)
       c.execute(query)
       results = c.fetchall()
       db.close()
@@ -32,6 +68,7 @@ class ReportRunner():
 
     def pretty_print(self, data):
         '''Takes data input and produces a readable output'''
+        print("----------------------------------")
         for row in data:
             print(row)
  
